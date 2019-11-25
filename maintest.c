9674828e9fcc5cbd23a134a21ca9847956a92122
shaharivka <57682267+shaharivka@users.noreply.github.com>
2019-11-25 15:12:53 +0200

    handle the part of 2 letters

--- maintest.c
+++ maintest.c
@@ -2,16 +2,21 @@
 #include "myBank.h"
 
 int main(){
-
+char option[2]={'!','!'};
 int status=0;
-char option;
+
 printf("Hey there! welcome to shaharivka's bank. Here are your following options:\n");
 printf(".O,I,P,E,C?....\n");
 while(status==0){
-    printf("enter your request:");
-    scanf(" %c",&option);
-    //option = getchar();
-    switch(option){
+   printf("enter your request:");
+   scanf(" %s",option); 
+   while(option[1]!='\0'){
+    printf("ERROR!,this is more then one letter, please enter again\n");
+    scanf(" %s",option); 
+   }
+    
+
+    switch(option[0]){
         
         case 'O':
          o(); 
@@ -49,6 +54,7 @@ while(status==0){
 
          default:
          printf("ERROR!,this is not one of the options, please enter again\n");
+         
          break;
 
         
